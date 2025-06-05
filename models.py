4a3e5eeca7783aafbe05c78d0a9425e5770aae33
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-06-05 19:30:47 -0300

    Modelo do Usuario

--- models.py
+++ models.py
@@ -1,3 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), unique = True, nullable = False)
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    password = db.Column(db.String(100), unique = True, nullable = False)
+    phone = db.Column(db.String(100), unique = True, nullable = False)
+    location = db.Column(db.String(100), unique = True, nullable = False)
+    description = db.Column(db.String(100), unique = True, nullable = False)
+
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'username' : self.username,
+            'email' : self.email,
+            'password' : self.password,
+            'phone' : self.phone,
+            'location' : self.location,
+            'description' : self.description
+        }
